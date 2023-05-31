d3dfa57de2d294ce8cd96d3ef7bac0e5899164f2
zero-two <miigo@duck.com>
2023-05-31 18:52:58 +0700

    entity

---
+++ src/main/java/franxx/code/reimiii/entity/CaseConverter.java
@@ -0,0 +1,20 @@
+package franxx.code.reimiii.entity;
+
+public class CaseConverter {
+    private String convertToCase;
+
+    public CaseConverter() {
+    }
+
+    public CaseConverter(String convertToCase) {
+        this.convertToCase = convertToCase;
+    }
+
+    public String getConvertToCase() {
+        return convertToCase;
+    }
+
+    public void setConvertToCase(String convertToCase) {
+        this.convertToCase = convertToCase;
+    }
+}
